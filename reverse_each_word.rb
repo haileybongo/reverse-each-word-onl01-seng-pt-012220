fd3757ef4d4239077d611dcc599c79d01d44b249
Hailey Bongo <haileyebongo@gmail.com>
2020-01-31 04:36:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   reverse_array = []
   array.collect do |word|
-    reverse_array << array.reverse
+    reverse_array << word.reverse
   end
   reverse_array.join(" ")
 end
